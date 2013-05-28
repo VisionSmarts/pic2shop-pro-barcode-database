911db00131aec160a2b562d949a3b842ced6966d
Benoit Maison <benoit.maison@visionsmarts.com>
2013-05-28 09:18:11 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,53 @@
+Pic2shop-pro-barcode-database
+=============================
+
+Single-file PHP database that integrates with mobile barcode scanner app *pic2shop® PRO* (Android and iOS).
+
+**This is not a barcode scanner.** This code demonstrates how to use pic2shop PRO to build a barcode-enabled online database.
+
+About pic2shop PRO
+------------------
+
+**Pic2shop PRO** is a paid app available for iOS and Android.  It reads EAN13, EAN8, UPC-A, UPC-E, Code 39, Code 128, ITF, (Interleaved 2 of 5) and Codabar (aka USD-4, NW-7) barcodes. After each scan, the app displays a user-defined url with the barcode string as parameter in the embedded web browser. EAN and UPC codes can be read with fixed-focus cameras, such as the iPod Touch 4th gen, the iPad2, the iPad front camera and many Android devices.
+
+[Pic2shop PRO] [1] is developed by [Vision Smarts] [2].
+
+Functionalities
+---------------
+
+The purpose of this sample code is to demonstrate how to integrate a web app with the mobile app pic2shop PRO. It uses PHP and SQLite but the same results can easily be achieved with other web frameworks.
+
+Automatic app configuration link (formats, Home page, Lookup page) via: 
+	p2spro://configure?
+
+Start a scan from a link in the page via:
+	p2spro://scan?
+
+Image upload via:
+	<input type="file" accept="image/*" capture="camera" name="image">
+
+Retrieve GET parameters when the Home or Lookup URLs are fetched.
+
+Retrieve POST parameters when the form is submitted.
+
+Suggestions for extensions
+--------------------------
+
+In real settings, a number of improvements would be needed:
+
+* Replace SQLite with MySQL or PostgreSQL
+* Organize the code in separate files (css, js, html view, php model+controller, etc)
+* Add a style sheet
+* Add better data validation
+* Add user authentication
+
+Trademarks
+----------
+
+IPHONE, IPOD and IPAD are trademarks of Apple Inc., registered in the U.S. and other countries. 
+IOS is a trademark or registered trademark of Cisco in the U.S. and other countries. 
+ANDROID is a trademark of Google Inc. 
+
+
+[1]: http://www.pic2shop.com/pro_version.html "pic2shop PRO"
+[2]: http://www.visionsmarts.com              "Vision Smarts"
---
+++ p2spro.php
@@ -0,0 +1,241 @@
+<?php
+
+### create or open the database or die
+try 
+{
+  # create or open the database, somewhere where we have write permission
+  $database = new SQLite3('myDatabase.sqlite3');
+}
+catch(Exception $e) 
+{
+  die('cannot create database, check path and permissions');
+}
+
+$query = 'create table if not exists Demo ' .
+'(ref TEXT PRIMARY KEY, format TEXT, gps TEXT, title TEXT, note TEXT, date DATE, info TEXT, imageType TEXT, imageData BLOB,  checkbox INTEGER)';
+if(!$database->query($query))
+  {
+    die('table not created');
+  }
+
+### get the self urls for configuration and callback
+$url = "http" . (($_SERVER['SERVER_PORT']==443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+$homeURL = urlencode($url.'?userid=1234');
+$lookupURL = urlencode($url.'?userid=1234&ref=CODE&format=FORMAT');
+							   
+### get parameters, init in case no record specified
+### will escape strings for SQL
+$userid = "";
+$ref = "";
+$format = "";
+$gps = "";
+$title = "";
+$note = "";
+$date = "";
+$imageType= "";
+$imageData= "";
+$checkbox = 0;
+$message = "";
+
+### GET request: home page or barcode lookup
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  if (isset($_GET['userid'])) {
+    $userid = sqlite_escape_string($_GET['userid']);;
+  }
+  if (isset($_GET['ref'])) {
+    $ref =  sqlite_escape_string($_GET['ref']);
+    if (isset($_GET['format'])) { $format =  sqlite_escape_string($_GET['format']); }
+    if (isset($_GET['gps'])) { $gps =  sqlite_escape_string($_GET['gps']); }
+    $query = "SELECT title, note, date, info, imageType, imageData, checkbox FROM Demo " .
+      "WHERE ref='".$ref."'";
+    $result = $database->query($query);
+    $record = $result->fetchArray(SQLITE3_ASSOC);
+    if (! $record) {
+      ### create record
+      $query = "INSERT INTO Demo (ref, format, gps, title, note, date, info, imageType, imageData, checkbox) VALUES ('$ref', '$format', '$gps', '$title', '$note', '$date', '$info', '$imageType' , '$imageData', '$checkbox')";
+      if (! $database->query($query)) {
+	$message = 'Error: record not created';
+      }
+    }
+  }
+}
+
+
+### POST request: form submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  # get all form data
+  $ref = sqlite_escape_string($_POST['ref']);
+  $format =  sqlite_escape_string($_POST['format']);
+  $gps =  sqlite_escape_string($_POST['gps']);
+  $title =  sqlite_escape_string($_POST['title']);
+  $note =  sqlite_escape_string($_POST['note']);
+  $date =  sqlite_escape_string($_POST['date']);
+  if (isset($_POST['checkbox'])) {
+    $checkbox = 1;
+  }
+  else {
+    $checkbox = 0;
+  }
+  if (isset($_FILES['image']['tmp_name'])) {
+    if (strlen($_FILES['image']['tmp_name'])>0) {
+      $imageType = sqlite_escape_string($_FILES['image']['type']);
+      $imageData = file_get_contents($_FILES['image']['tmp_name']);
+      # limit image size to 400px
+      list($width, $height) = getimagesize($_FILES['image']['tmp_name']);
+      if (max($width, $height)>400) {
+	$scale = 400.0/max($width, $height);
+	$new_width = $width * $scale;
+	$new_height = $height * $scale;
+	$image = imagecreatefromstring($imageData);
+	$image_scaled = imagecreatetruecolor($new_width, $new_height);
+	imagecopyresampled($image_scaled, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+	ob_start();
+	imagepng($image_scaled);
+	$imageData = ob_get_contents(); 
+	ob_end_clean();
+	$imageType = "image/png";
+	imagedestroy($image);
+	imagedestroy($image_scaled);
+	echo 'resized by '.$scale;
+      }
+      $imageData = base64_encode($imageData); // sqlite_escape_string($imageData);
+    }
+  }
+  # update record
+  $query = "SELECT title, note, date, info, imageType, imageData, checkbox FROM Demo " .
+    "WHERE ref='".$ref."'";
+  $result = $database->query($query);
+  $record = $result->fetchArray(SQLITE3_ASSOC);
+  if ($record) {
+    # update everything but image
+    $query = "UPDATE Demo SET format='$format', gps='$gps', title='$title', note='$note', date='$date', info='$info', checkbox='$checkbox' WHERE ref='$ref'";
+    if (! $database->query($query)) {
+      $message = 'Error: record not updated';
+    }
+    # update image if provided
+    if (strlen($imageData)>0) {
+      $query = "UPDATE Demo SET imageType='$imageType' , imageData='$imageData' WHERE ref='$ref'";
+      if (! $database->query($query)) {
+	$message = 'Error: image not updated';
+      }
+    }
+  }
+  else {
+    # create record
+    $query = "INSERT INTO Demo (ref, format, gps, title, note, date, info, imageType, imageData, checkbox) VALUES ('$ref', '$format', '$gps', '$title', '$note', '$date', '$info', '$imageType' , '$imageData', '$checkbox')";
+    if (! $database->query($query)) {
+      $message = 'Error: record not created';
+    }
+  }
+}
+
+### get updated record data for display
+if (strlen($ref)>0) {
+  $query = "SELECT title, note, date, info, imageType, imageData, checkbox FROM Demo " .
+    "WHERE ref='".$ref."'";
+  $result = $database->query($query);
+  $record = $result->fetchArray(SQLITE3_ASSOC);
+  if ($record) {
+    $title = $record['title'];
+    $note = $record['note'];
+    $date = $record['date'];
+    $info = $record['info'];
+    $imageType = $record['imageType'];
+    $imageData = $record['imageData'];
+    $checkbox = $record['checkbox'];
+  }
+}
+				
+### escape strings for display	   
+$title = htmlspecialchars($title);
+$note = htmlspecialchars($note);
+$date = htmlspecialchars($date);
+$info = htmlspecialchars($info);
+$imageType = htmlspecialchars($imageType);
+$checkbox = htmlspecialchars($checkbox);
+?>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="format-detection" content="telephone=no">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>          
+<script type="text/javascript">
+// this function can be called by pic2shop pro to insert a scan (specified in callback=)
+function insertCodeFormat(code,format) {
+   $("textarea#scanresult").text(decodeURIComponent(code));
+}
+</script></head>
+<body>
+
+<p>DEMO ONLY! The data you insert can be viewed and changed by anyone, and will be erased at least once a day.</p>
+
+<p>The source code of this demo is at: <a href="https://github.com/VisionSmarts/pic2shop-pro-barcode-database">https://github.com/VisionSmarts/pic2shop-pro-barcode-database</a></p>
+
+<?php 
+  if ($userid=="") 
+    {
+      # auto-configuration URL. Works from the default browser and the in-app browser
+      # configures the scanner to decode everything but QR
+      # and to fetch this page at start (home=) and after a scan (lookup=)
+      echo "<p><a href='p2spro://configure?formats=EAN13,EAN8,UPCE,ITF,CODE39,CODE128,CODABAR&gps=TRUE&home={$homeURL}&lookup={$lookupURL}'>Click here</a> to configure pic2shop PRO for this demo: (to clear, go to Configuration and delete both Home and Lookup URLs)</p>";
+      die;
+    }
+?>
+
+<!-- error message if any -->
+<h3 style="color:red"><?= $message; ?></h3>
+				 
+<!-- starts a scan, can override the formats, gps and lookup url if needed -->
+<p><a href="p2spro://scan?formats=EAN13,EAN8,UPCE,ITF,CODE39,CODE128,CODABAR&gps=TRUE&callback=<?= $lookupURL; ?>">
+Lookup another barcode</a>
+</p>
+
+<h2>Barcode <?= $ref; ?> (<?= $format; ?>)</h2>
+Last scanned at <?= $gps; ?>
+
+<form action="pic2shop_scan.php" enctype="multipart/form-data" method="post">
+<input type="hidden" name="ref" value="<?= $ref; ?>">
+<input type="hidden" name="format" value="<?= $format; ?>">
+<input type="hidden" name="gps" value="<?= $gps; ?>">
+
+<p>
+Text:<br>
+<input type="text" name="title" value="<?= $title ?>" size="30">
+</p>
+
+<p>
+<!-- javascript callback to the 'insertCodeFormat' function defined above 
+  scan QR only -->
+Note (<a href="p2spro://scan?formats=QR&callback=javascript:insertCodeFormat('CODE','FORMAT')">scan QR code to fill in</a>):<br>
+<textarea id="scanresult" rows="5" cols="30" name="note"><?= $note ?></textarea>
+</p>
+
+<p>
+Photo: 
+<?
+  if (strlen($imageData)>0) {
+    $imgsrc = "data:$imageType;base64,$imageData";
+    echo "<img style='max-height: 200px; max-width: 200px' src='$imgsrc' />";
+  }
+?>
+<br>
+<!-- image upload is supported, but not other data types -->
+<input type="file" accept="image/*" capture="camera" name="image">
+</p>
+
+<p>
+Date:<br>
+<input type="date" name="date" value="<?= $date ?>">
+</p>
+
+<p>
+Checkbox:<br>
+<input type="checkbox" name="checkbox" <?= ($checkbox==1)?'checked':''; ?>>
+</p>
+
+<input type="submit" value="Save">
+
+</form>
+
+</body>
+</html>
